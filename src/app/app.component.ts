dd3eb5241ab00fbf29d61f32db23b2660414f1fa
Alaska94 <stephen.alaska94@gmail.com>
2023-08-23 19:41:39 +0000

    remove daisyui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { initFlowbite, initDropdowns } from 'flowbite';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'scribble-ui';
+
+  ngOnInit(): void {
+    initFlowbite();
+    initDropdowns();
+  }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,ts}"],
+  content: ["./src/**/*.{html,ts}", "./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {
       colors: {
@@ -9,5 +9,5 @@ module.exports = {
       },
     },
   },
-  plugins: [require("daisyui")],
+  plugins: [require("flowbite/plugin")],
 };
